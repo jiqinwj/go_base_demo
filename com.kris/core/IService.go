2e93bc677354bdaaa7e9ba7230f34116aeb171ca
kris_ji <958940766@qq.com>
2020-01-16 14:06:08 +0800

    并发处理

--- com.kris/core/IService.go
+++ com.kris/core/IService.go
@@ -1,16 +1,14 @@
 package core
 
 type IService interface {
-    Get(id int) string
-
+	Get(id int) string
 }
 
 var myService IService
 
-func  SetService(service IService)  {
-    myService=service
+func SetService(service IService) {
+	myService = service
 }
 func GetService() IService {
-    return myService
+	return myService
 }
-

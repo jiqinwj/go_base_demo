9202e23c1eb6295b25f8232257bfad0f0195d0ed
kris_ji <958940766@qq.com>
2020-01-16 10:15:11 +0800

    context包被称为上下文包，go1.7加入，用于协程之间的上下文数据的传递、中止和控制超时。在网络编程中可用于请求的中止，譬如服务访问链的中止：

--- com.kris/mytest/main.go
+++ com.kris/mytest/main.go
@@ -1,23 +1,65 @@
 package main
 
 import (
-	"regexp"
+	"context"
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
-func GetRqPath(rq string) string  {
-	r:=regexp.MustCompile(`^GET\s(.*?)\sHTTP`)
-	if r.MatchString(rq) {
-		return r.FindStringSubmatch(rq)[1]
-	} else{
-		return "/"
+//TODO 正则乱入
+
+//func GetRqPath(rq string) string  {
+//	r:=regexp.MustCompile(`^GET\s(.*?)\sHTTP`)
+//	if r.MatchString(rq) {
+//		return r.FindStringSubmatch(rq)[1]
+//	} else{
+//		return "/"
+//	}
+//}
+//func main()  {
+//
+//	str:=`GET /abc.php HTTP/1.1
+//Host: localhost:8099
+//Connection: keep-alive
+//Upgrade-Insecure-Requests: 1`
+//	fmt.Println(GetRqPath(str))
+//}
+
+
+
+func main()  {
+	rand.Seed(time.Now().Unix())
+
+	ctx,_:=context.WithTimeout(context.Background(),time.Second*3)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go GenUsers(ctx,&wg)
+	wg.Wait()
+
+	fmt.Println("生成幸运用户成功")
+}
+func GenUsers(ctx context.Context,wg *sync.WaitGroup)  { //生成用户ID
+	fmt.Println("开始生成幸运用户")
+	users:=make([]int,0)
+guser:for{
+	select{
+	case <- ctx.Done(): //代表父context发起 取消操作
+
+		fmt.Println(users)
+		wg.Done()
+		break guser
+		return
+	default:
+		users=append(users,getUserID(1000,100000))
 	}
 }
-func main()  {
 
-	str:=`GET /abc.php HTTP/1.1
-Host: localhost:8099
-Connection: keep-alive
-Upgrade-Insecure-Requests: 1`
-	fmt.Println(GetRqPath(str))
 }
+func getUserID(min int ,max int) int  {
+	return rand.Intn(max-min)+min
+}
+
+
